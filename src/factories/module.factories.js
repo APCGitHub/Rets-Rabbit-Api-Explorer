efac58783bd99f761fd6b7623719fcb5b5cd50de
Andrew Clinton <aclinton@anecka.com>
2016-06-08 17:19:25 -0400

    moving all factories and service to external angular project to make things more modular

--- src/factories/module.factories.js
+++
@@ -1,9 +0,0 @@
-(function () {
-	'use strict';
-
-	angular
-		.module('rr.api.v2.explorer.factories', [
-			'rr.api.v2.explorer.factory.auth',
-			'rr.api.v2.explorer.factory.properties'
-		]);
-})();
