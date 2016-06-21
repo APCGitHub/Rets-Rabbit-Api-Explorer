b25ca587ada96a8167098722a7c57a78e71a6d38
Andrew Clinton <aclinton@anecka.com>
2016-06-21 15:26:00 -0400

    try one way again

--- src/directives/explorer.directive.js
+++ src/directives/explorer.directive.js
@@ -10,12 +10,12 @@
         return {
             restrict: 'EA',
             scope: {
-                woah: '=search'
+                search: '@'
             },
             link: function(scope, element, attrs, controller) {
-                console.log('search: ' + scope.woah);
+                console.log('search: ' + scope.search);
                 // detect outside changes and update our input
-                scope.$watch('woah', function(newv) {
+                scope.$watch('search', function(newv) {
                     console.log(newv);
                 });
             },
