9c43099333e4a0cb81c7348cf8ed5a200e68e01c
Andrew Clinton <aclinton@anecka.com>
2016-06-22 11:35:23 -0400

    speed it up even more

--- src/directives/explorer.directive.js
+++ src/directives/explorer.directive.js
@@ -202,7 +202,7 @@
             function _startCount(total_time) {
                 var time = 0;
                 vm.data.query_time = 0;
-                var max_time = 1750;
+                var max_time = 1250;
                 promise = $interval(function(time) {
                     if (vm.data.query_time == total_time) {
 
