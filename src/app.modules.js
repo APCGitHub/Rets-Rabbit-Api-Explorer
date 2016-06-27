214d0e492db4b1d2fc6f291ac8f72a4dfecbcc95
Andrew Clinton <aclinton@anecka.com>
2016-06-27 15:26:37 -0400

    make sure we pull in the explorer module

--- src/app.modules.js
+++ src/app.modules.js
@@ -2,6 +2,7 @@
 	'use strict';
 	
 	angular.module('rr.api.v2.explorer', [
+		'rets-rabbit-angular',
 		'rr.api.v2.explorer.directives',
 		'rr.api.v2.explorer.filters',
 		'rr.api.v2.explorer.templates'
