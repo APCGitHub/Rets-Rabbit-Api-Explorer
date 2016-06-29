74a724054666b7263cf77f55fc08a7fc0f7d789e
Andrew Clinton <aclinton@anecka.com>
2016-06-29 09:41:08 -0400

    require nganimate

--- src/app.modules.js
+++ src/app.modules.js
@@ -3,6 +3,7 @@
 	
 	angular.module('rr.api.v2.explorer', [
 		'rets-rabbit-angular',
+		'ngAnimate',
 		'rr.api.v2.explorer.directives',
 		'rr.api.v2.explorer.filters',
 		'rr.api.v2.explorer.templates'
