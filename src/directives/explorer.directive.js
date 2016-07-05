007503d9dcca86c5053e4b2d99007396cf0e59df
Andrew Clinton <aclinton@anecka.com>
2016-07-05 12:55:49 -0400

    rest of leaflet draw logics

--- src/directives/explorer.directive.js
+++ src/directives/explorer.directive.js
@@ -267,6 +267,35 @@
                     }, interval);
                 }
             }
+
+            //LEAFLET controls
+            var handle = {
+                created: function(e, leafletEvent, leafletObject, model, modelName) {
+                    drawnItems.addLayer(leafletEvent.layer);
+                },
+                edited: function(arg) {},
+                deleted: function(arg) {
+                    var layers;
+                    layers = arg.layers;
+                    drawnItems.removeLayer(layer);
+                },
+                drawstart: function(arg) {},
+                drawstop: function(arg) {},
+                editstart: function(arg) {},
+                editstop: function(arg) {},
+                deletestart: function(arg) {},
+                deletestop: function(arg) {}
+            };
+            var drawEvents = leafletDrawEvents.getAvailableEvents();
+            drawEvents.forEach(function(eventName) {
+                $scope.$on('leafletDirectiveDraw.' + eventName, function(e, payload) {
+                    //{leafletEvent, leafletObject, model, modelName} = payload
+                    var leafletEvent, leafletObject, model, modelName; //destructuring not supported by chrome yet :(
+                    leafletEvent = payload.leafletEvent, leafletObject = payload.leafletObject, model = payload.model,
+                        modelName = payload.modelName;
+                    handle[eventName.replace('draw:', '')](e, leafletEvent, leafletObject, model, modelName);
+                });
+            });
         }];
 
         return {
