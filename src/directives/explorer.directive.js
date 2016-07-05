d9a98c3edb37c115d15f0770d134cf69563a2c88
Andrew Clinton <aclinton@anecka.com>
2016-07-05 14:22:25 -0400

    trim down

--- src/directives/explorer.directive.js
+++ src/directives/explorer.directive.js
@@ -7,15 +7,15 @@
     Directive.$inject = ['ApiConfig', 'PropertyFactory'];
 
     function Directive(ApiConfig, PropertyFactory) {
-        var controller = ['$scope', '$interval', '$document', 'PropertyFactory', /*'leafletData', 'leafletDrawEvents',*/ 'uiGmapGoogleMapApi', function($scope, $interval, $document, PropertyFactory, /*leafletData, leafletDrawEvents,*/ uiGmapGoogleMapApi) {
+        var controller = ['$scope', '$interval', '$document', 'PropertyFactory', /*'leafletData', 'leafletDrawEvents', 'uiGmapGoogleMapApi',*/ function($scope, $interval, $document, PropertyFactory /*leafletData, leafletDrawEvents, uiGmapGoogleMapApi*/) {
             var vm = this,
                 promise,
                 someElement = angular.element(document.getElementById('rr-query-results'));
             //drawnItems = new L.FeatureGroup();
 
-            uiGmapGoogleMapApi.then(function(maps) {
-                console.log('hallo');
-            });
+            // uiGmapGoogleMapApi.then(function(maps) {
+            //     console.log('hallo');
+            // });
 
             //Watch for when the search attribute value changes from the parent scope
             $scope.$watch(angular.bind(this, function() {
