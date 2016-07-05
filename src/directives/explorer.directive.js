aa2b5f030b73570f3ddf038f6a58fc7d0eb69a67
Andrew Clinton <aclinton@anecka.com>
2016-07-05 14:41:44 -0400

    drawing starting

--- src/directives/explorer.directive.js
+++ src/directives/explorer.directive.js
@@ -51,7 +51,30 @@
                         lat: 39.9612,
                         lng: -82.9988
                     },
-                    zoom: 10
+                    zoom: 10,
+                    drawingManagerOptions: {
+                        drawingMode: google.maps.drawing.OverlayType.MARKER,
+                        drawingControl: true,
+                        drawingControlOptions: {
+                            position: google.maps.ControlPosition.TOP_CENTER,
+                            drawingModes: [
+                                google.maps.drawing.OverlayType.MARKER,
+                                google.maps.drawing.OverlayType.CIRCLE,
+                                google.maps.drawing.OverlayType.POLYGON,
+                                google.maps.drawing.OverlayType.POLYLINE,
+                                google.maps.drawing.OverlayType.RECTANGLE
+                            ]
+                        },
+                        circleOptions: {
+                            fillColor: '#ffff00',
+                            fillOpacity: 1,
+                            strokeWeight: 5,
+                            clickable: false,
+                            editable: true,
+                            zIndex: 1
+                        }
+                    },
+                    drawingManagerControl: {}
                 },
                 fullRequest: ApiConfig.apiUrl + 'property?',
                 request: '',
